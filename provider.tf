a9eab977ce66d08aafda39264e99ec92af3412e4
Steve Lane <sclane68@yahoo.co.uk>
2024-05-13 22:08:11 +0100

    remote state

--- provider.tf
+++ provider.tf
@@ -1,5 +1,12 @@
 terraform {
 
+  backend "s3" {
+    bucket                  = "scl-terraform-s3-state"
+    key                     = "aws-ec2-basic"
+    region                  = "eu-west-2"
+    shared_credentials_file = "~/.aws/credentials"
+  }
+
   required_version = ">= 1.0"
 
   required_providers {
